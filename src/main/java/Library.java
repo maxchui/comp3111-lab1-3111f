bad6fdda18d5c77f4f3d56ef4f57b54a4ca0b04c
maxchui <11816568+maxchui@users.noreply.github.com>
2020-09-14 14:21:28 +0800

    A great person called Desmond once said, "No hehe, but haha."

---
+++ src/main/java/Library.java
@@ -0,0 +1,12 @@
+public class Library {
+    public static void main(String[] args) {
+        Library lib = new Library();
+        if(lib.someMethod()){
+            System.out.println ("No hehe, but haha (Tsoi, 2019)");
+        }
+    }
+
+    public boolean someMethod(){
+        return true;
+    }
+}
